deb6691a9766b801256412df369e9fad80b61ce1
skswldndi <86106509+skswldndi@users.noreply.github.com>
2025-11-29 18:27:35 +0000

    Deploying to gh-pages from @ skswldndi/skswldndi.github.io@f646855fbe91f52c659b61c42c05e237241da3d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A collection of projects spanning data, NLP x HCI, and creative explorations for fun.",
+          description: "A collection of projects spanning NLP x HCI and creative explorations for fun.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio/projects/";
